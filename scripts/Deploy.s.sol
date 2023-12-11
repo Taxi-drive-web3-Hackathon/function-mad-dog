ba02984041873ac227352d65d00af2fd912b9735
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-12-10 22:15:29 -0300

    update deploy

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -10,8 +10,9 @@ import {CCIPAdapter} from "../src/CCIPAdapter.sol";
 
 contract PinGoScript is Script {
 
-    address token = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-		address router = 0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C;
+		address link = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    address token = 0xf1E3A5842EeEF51F2967b3F05D45DD4f4205FF40;
+		address routerCCIP = 0x1035CabC275068e0F4b745A29CEDf38E13aF41b1;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -22,18 +23,17 @@ contract PinGoScript is Script {
 				Vault vault = new Vault();
 
 				vault.initialize(address(pin));
-				ERC20(token).approve(address(vault), 4000000000000000000);
-				vault.deposit(token, 4000000000000000000);
+				ERC20(token).approve(address(vault), 1000000000);
+				vault.deposit(token, 1000000000);
 
-				adapter.initialize(token, address(pin), router);
+				adapter.initialize(token, address(pin), routerCCIP);
 
 				adapter.allowlistDestinationChain(12532609583862916517, true);
 				adapter.allowlistDestinationChain(14767482510784806043, true);
+				
+				ERC20(link).transfer(address(adapter), 4000000000000000000);
 
-				ERC20(token).approve(address(adapter), 4000000000000000000);
-				ERC20(token).transfer(address(adapter), 4000000000000000000);
-
-				ApiConsumer consumer = new ApiConsumer(router);
+				ApiConsumer consumer = new ApiConsumer(routerCCIP);
 				consumer.setPing(address(pin));
 
         pin.addReceiver(1, 0x6aC4DB972e2c94343b7Dc14c1AEaCc2cC1a3e05d);
